fa04ae0408b7155939219dc4d876982f2b8baed8
JoaoGDFaria <joaofaria13@gmail.com>
2023-10-29 17:06:33 +0000

    Timer de 3 segundos feito, operações sobre as hashs

--- FS_Tracker.java
+++ FS_Tracker.java
@@ -1,37 +1,104 @@
+import java.time.Duration;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class FS_Tracker {
     private Map<String, Map<Integer, List<String>>> fileMemory;
+    private Map<String, LocalTime> timeStamps;
 
     public FS_Tracker(){
         this.fileMemory = new HashMap<>();
+        this.timeStamps = new HashMap<>();
     }
 
-    public int insert_info(String fileName, Integer blockNumber, String ipNode){
 
-        if (!fileMemory.containsKey(fileName)) {
-            fileMemory.put(fileName, new HashMap<>());
-        }
+    public int insertInfo(String fileName, Integer blockNumber, String ipNode){
 
+        LocalTime now = LocalTime.now();
+
+        insertTimeStamps(now, ipNode);
+
+        if (!fileMemory.containsKey(fileName)) fileMemory.put(fileName, new HashMap<>());
         Map<Integer, List<String>> blockMap = fileMemory.get(fileName);
 
-        if (!blockMap.containsKey(blockNumber)) {
-            blockMap.put(blockNumber, new ArrayList<>());
-        }
-
+        if (!blockMap.containsKey(blockNumber)) blockMap.put(blockNumber, new ArrayList<>());
         List<String> ipList = blockMap.get(blockNumber);
 
         ipList.add(ipNode);
-
         blockMap.put(blockNumber, ipList);
-
         fileMemory.put(fileName, blockMap);
 
-        return 0;
+        return 1;
+
     }
 
-    @Override
+    public void insertTimeStamps(LocalTime time, String ipNode){
+        this.timeStamps.put(ipNode, time);
+    }
+
+    public void sendIPBack(String fileName, Integer blockNumber){
+
+        Map<Integer, List<String>> blockMap = fileMemory.get(fileName);
+        List<String> IPs = blockMap.get(blockNumber);
+
+        if(IPs.size()>1) {
+            String used = IPs.get(0);
+            IPs.remove(0);
+            IPs.add(used);
+        }
+
+        blockMap.put(blockNumber, IPs);
+        fileMemory.put(fileName, blockMap);
+    }
+
+    public void deleteDisconnectedNode(String ipDisc){
+        int cont = 0;
+        for (Map.Entry<String, Map<Integer, List<String>>> entry : fileMemory.entrySet()){
+            String fileName = entry.getKey();
+            Map<Integer, List<String>> blockMap = entry.getValue();
+
+            for (Map.Entry<Integer, List<String>> blockEntry : blockMap.entrySet()){
+                Integer blockNumber = blockEntry.getKey();
+                List<String> ipList = blockEntry.getValue();
+                ipList.remove(ipDisc);
+            }
+        }
+
+        this.timeStamps.remove(ipDisc);
+    }
+
+    public void verifyTimeStamp(){
+        LocalTime now = LocalTime.now();
+
+        for(Map.Entry<String, LocalTime> entry : timeStamps.entrySet()){
+            LocalTime before = entry.getValue();
+            Duration duration = Duration.between(before, now);
+            long secondsDifference = duration.getSeconds();
+
+            if(secondsDifference>3) deleteDisconnectedNode(entry.getKey());
+        }
+    }
+
+
     public String toString() {
-        return this.fileMemory.get("A").get(1).toString();
+
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, Map<Integer, List<String>>> entry : fileMemory.entrySet()) {
+            String fileName = entry.getKey();
+            Map<Integer, List<String>> blockMap = entry.getValue();
+
+            for (Map.Entry<Integer, List<String>> blockEntry : blockMap.entrySet()) {
+                Integer blockNumber = blockEntry.getKey();
+                List<String> ipList = blockEntry.getValue();
+
+                result.append("File: ").append(fileName)
+                        .append(", Block: ").append(blockNumber)
+                        .append(", IPs: ").append(ipList).append("\n");
+            }
+        }
+
+        return result.toString();
     }
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,28 @@
+import java.time.LocalTime;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class Main {
     public static void main(String[] args){
+
         FS_Tracker fs = new FS_Tracker();
-        fs.insert_info("A",1,"A");
-        fs.toString();
+        fs.insertInfo("A",1,"A");
+        fs.insertInfo("A",1,"B");
+        fs.insertInfo("S",1,"A");
+        fs.insertInfo("S",1,"B");
+        fs.insertInfo("D",1,"E");
+
+        Timer timer = new Timer();
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                fs.verifyTimeStamp();
+            }
+        };
+
+        timer.scheduleAtFixedRate(task, 0, 3000);
+
+        System.out.println(fs.toString());
     }
 }
