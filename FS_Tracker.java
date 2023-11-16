338930f83485e653714a6a98cd4ca3f94af209ef
joaohcf <joaoh.c.f.39@hotmail.com>
2023-11-16 10:43:49 +0000

    node handler

--- FS_Tracker.java
+++ FS_Tracker.java
@@ -12,71 +12,6 @@ import java.util.TimerTask;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 /**
- * CLASS HANDLER - Trata de pedidos de um cliente
- */
-class Handler extends Thread{
-    private Socket socket;                   // Conecção entre cliente e servidor
-    private BufferedWriter bufferedToNode;   // Escrever informação enviada para o cliente
-    private BufferedReader bufferedFromNode; // Ler informação enviada pelo cliente
-    private FS_Tracker tracker;
-    private String ipAddress;
-
-    public Handler(Socket socket, FS_Tracker tracker) throws IOException{
-        this.socket = socket;
-        this.bufferedToNode = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream())); // Enviar 
-        this.bufferedFromNode = new BufferedReader(new InputStreamReader(socket.getInputStream())); // Receber
-        this.tracker = tracker;
-        String messageReceived = bufferedFromNode.readLine();
-        this.ipAddress = tracker.ipAdressNode(messageReceived);
-
-        System.out.println("Node " + ipAddress + " is connected.");
-        //System.out.println(messageReceived);  // COLOCAR ATIVO PARA DEMONSTRAR
-        tracker.messageParser(messageReceived);
-    }
-
-    public void run(){
-        try{
-            while(socket.isConnected()){
-                String req = bufferedFromNode.readLine();
-
-                if(req == null){
-                    bufferedFromNode.close();
-                    bufferedToNode.close();
-                    socket.close();
-                    break;
-                }
-
-                // Disconnect node from FSTracker
-                if (req.charAt(0) == 'd'){
-                    this.tracker.deleteDisconnectedNode(this.ipAddress);
-                    break;
-                }
-                else{
-                    if (req.charAt(0) == 'i'){
-                        this.tracker.memoryToString();
-                        System.out.println("##############");
-                        this.tracker.timeToString();
-                        System.out.println("##############");
-                        System.out.println("##############");
-                        System.out.println("##############\n");
-                    }else if (req.length() >= 5 && req.startsWith("GET ")){
-                        String messageToNode = this.tracker.pickFile(req.substring(4), bufferedToNode);
-                        this.tracker.sendInfoToNode(messageToNode,bufferedToNode);
-                    }else{
-                        this.tracker.messageParser(req);
-                        //System.out.println(req);  // COLOCAR ATIVO PARA DEMONSTRAR
-                    }
-                    //System.out.println(req); 
-                }
-
-            }
-        }catch(IOException e){
-            System.out.println("ERRO HANDLER: " + e.getMessage());
-        }
-    }
-}
-
-/**
  * CLASS FS_TRACKER - Servidor tracker
  */
 public class FS_Tracker {
@@ -102,7 +37,7 @@ public class FS_Tracker {
         checkAlive();
 
         while (!tracker_socket.isClosed()) {
-            new Handler(tracker_socket.accept(), this).start();
+            new Node_Handler(tracker_socket.accept(), this).start();
         }
     }
 
